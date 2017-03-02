fdb07d09d8c0f2f487610693efd813c285cd86d7
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.0.194>
2017-03-02 20:07:57 -0300

    Versao 1.0 = versao inicial

---
+++ Teste/src/opet/AloMundo.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author Lab. Desenvolvimento
+ *
+ */
+package opet;
+
+public class AloMundo{
+    public static void main(String[] args) {
+        System.out.println("Alo Mundo");
+    } 
+}
